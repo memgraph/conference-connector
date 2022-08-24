34be2ff7981085d1db881a7204c476a30e49fac9
antejavor <ante.javor@memgraph.io>
2022-08-24 19:44:32 +0200

    Update.

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,28 @@
 from typing import Union
 from fastapi import FastAPI
-from tweepy import Client
-from gqlalchemy import Memgraph, Match, Call
-from utils import env
+from gqlalchemy import Match, Call
+from models import memgraph
+from twitter_data import init_db_from_twitter, init_twitter_access, get_all_nodes_and_relationships
 import logging
 import os 
 import time
 
-
-twitter_client = Client(bearer_token=None)
-memgraph = Memgraph()
 log = logging.getLogger(__name__)
 app = FastAPI()
-
 
 def init_log():
     logging.basicConfig(level=logging.DEBUG)
     log.info("Logging enabled")
 
 
-def init_twitter_access():
-    bearer_token = env.get_required_env('BEARER_TOKEN')
-    twitter_client.bearer_token = bearer_token
-        
-
 def connect_to_memgraph():
+    
     connection_established = False
     while not connection_established:
         try:
             if memgraph._get_cached_connection().is_active():
                 connection_established = True
+                log.info("Connected to memgraph.")
         except:
             log.info("Memgraph probably isn't running.")
             time.sleep(4)
@@ -41,16 +34,19 @@ def startup_event():
     init_log()
     init_twitter_access()
     connect_to_memgraph()
-    twitter_api_helper()
+    #init_db_from_twitter()
     
 @app.get("/")
 def read_root():
      return {"Hello": "World"}
 
-#Get whole graph
 
 
-#Get ranking based on PageRank
+@app.get("/graph")
+async def get_graph():
+    return get_all_nodes_and_relationships()
+
+
 
 
 
--- backend/twitter_data.py
+++ backend/twitter_data.py
@@ -1,31 +1,111 @@
-from main import memgraph
-from main import twitter_client, log
-from datetime import datetime, timedelta
 import traceback
+from utils import env
+from tweepy import Client
+from models import memgraph, Tweet, Participant, Tweeted
+from gqlalchemy import Match
+from datetime import datetime, timedelta
+import json
+
+twitter_client = Client(bearer_token=None)
 
 hashtag = "#memgraph"
+user = "kgolubic"
 
-def twitter_api_helper():
-    twitter_client.bearer_token = ""
-    latest_tweets_with_hashtag(hashtag, days=1, hours=0)
+def init_twitter_access():
+    bearer_token = env.get_required_env('BEARER_TOKEN')
+    twitter_client.bearer_token = bearer_token
+        
 
-    # id = user.data["id"]
-    # tweets = twitter_client.get_users_tweets(id=id, tweet_fields=['context_annotations','created_at','geo'])
-
-    # for tweet in tweets.data:
-    #     print(tweets.data)
-    #     print(tweet)
-
-    
-def latest_tweets_with_hashtag(hashtag: str, days: int = 0, hours: int = 1):
+def get_latest_tweets_with_hashtag(hashtag: str, days: int = 0, hours: int = 1):
     try: 
+        tweets = {}
         last_hour = datetime.utcnow() - timedelta(days=days, hours = hours)
-        tweets = twitter_client.search_recent_tweets(query=hashtag + " -is:retweet", start_time=last_hour)
-        for tweet in tweets.data:
-            print(str(tweet.id) + tweet.text)
+        request = twitter_client.search_recent_tweets(query=hashtag + " -is:retweet", start_time=last_hour, tweet_fields=['context_annotations', 'created_at'], user_fields=['profile_image_url'], expansions=["author_id"])
+        users = {u["id"]: u for u in request.includes['users']}
+        for tweet in request.data:
+            if users[tweet.author_id]:
+                user = users[tweet.author_id]
+                #print(str(tweet.author_id) + " " + str(tweet.id) + " " + tweet.text)
+                tweets[tweet.id] = {
+                    'id' : tweet.id,
+                    'text' : tweet.text,
+                    'created_at' : str(tweet.created_at),
+                    'participant_id' : user.id,
+                    'participant_name' : user.name,
+                    'participant_username' : user.username,
+                }
+        return tweets
+    except Exception as e: 
+        traceback.print_exc()
+ 
+def save_tweets_and_participant(tweets):
+    for key, tweet in tweets.items(): 
+        tweet_node = Tweet(
+            id = tweet['id'],
+            text = tweet['text'],
+            created_at = tweet['created_at']
+        ).save(memgraph)
+        participant_node = Participant(
+            id = tweet['participant_id'],
+            name = tweet['participant_name'],
+            username = tweet['participant_username'],
+        ).save(memgraph)
+
+        tweeted_rel = Tweeted(
+            _start_node_id=participant_node._id, _end_node_id=tweet_node._id
+        ).save(memgraph)
+
+
+def save_participant(participant):
+    participant_node = models.Participant(
+        id = participant['id'],
+        name = participant['name'],
+        username = participant['username'],
+    )
+    print(participant_node)
+
+def get_participant_by_username(username: str):
+    try: 
+        request = twitter_client.get_user(username=username)
+        participant = {
+            'id' : request.data.id,
+            'name' : request.data.name,
+            'username' : request.data.username,
+        }
+        return participant
     except Exception as e: 
         traceback.print_exc()
 
+def get_all_nodes_and_relationships():
+
+    try:
+        results = list(
+            Match()
+            .node(labels="Participant", variable="p")
+            .to("TWEETED", variable="r")
+            .node(labels="Tweet", variable="t")
+            .return_()
+            .execute()
+        )
+
+        graph = list()
+        for result in results:
+            graph.append(result['p'])
+            graph.append(result['r'])
+            graph.append(result['t'])
+    
+            
+        return graph
+
+    except Exception as e:
+        traceback.print_exc()
+        return ("", 500)
 
 
-twitter_api_helper()
+
+def init_db_from_twitter():
+    memgraph.drop_database()
+    tweets = get_latest_tweets_with_hashtag(hashtag, days=7, hours=0)
+    save_tweets_and_participant(tweets)
+
+
